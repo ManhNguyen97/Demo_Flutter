1109b441e9703d63a0a1817f0541a3e1a446aca4
ManhNguyen <tienmanh97hy@gmail.com>
2022-01-25 17:01:05 +0700

    Post my_app_01

---
+++ my_app_01/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Xin chao!"),
+    );
+  }
+}
